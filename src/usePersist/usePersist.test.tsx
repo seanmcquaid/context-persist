dbf7fca35b9c971e4302659192fda1c79fbdb088
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-09 23:27:22 -0400

    fixing test config and wrote tests for usePersist

---
+++ src/usePersist/usePersist.test.tsx
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest';
+import { renderHook } from '@testing-library/react';
+import { usePersist } from './index';
+import { PersistProvider } from '../PersistProvider';
+
+describe('usePersist', () => {
+  it('throws an Error if the hook is not used within the PersistProvider', () => {
+    try {
+      renderHook(() => usePersist());
+    } catch (e) {
+      expect(e).not.toBeNull();
+    }
+  });
+  it('returns a value if the hook is used within the PersistProvider', () => {
+    const { result } = renderHook(() => usePersist(), {
+      wrapper: ({ children }) => (
+        <PersistProvider
+          defaultValue={'value'}
+          persistKey={''}
+          persistVersion={1}
+        >
+          {children}
+        </PersistProvider>
+      ),
+    });
+    expect(result.current.value).toEqual('value');
+  });
+});
