d7f220ceff97907f237bf82b8f2c032e04556996
seanmcquaid <sean.m.mcquaid@gmail.com>
2022-10-09 21:34:30 -0400

    continuing first iteration work

--- src/PersistProvider/index.tsx
+++ src/PersistProvider/index.tsx
@@ -8,8 +8,8 @@ import {
   useMemo,
 } from 'react';
 
-export const PersistValueContext = createContext<any | undefined>(undefined);
-export const PersistUpdateValueContext = createContext<
+const PersistValueContext = createContext<any | undefined>(undefined);
+const PersistUpdateValueContext = createContext<
   Dispatch<SetStateAction<any>> | undefined
 >(undefined);
 
@@ -17,19 +17,40 @@ interface PersistProviderProps {
   children: ReactNode;
   defaultValue: any;
   persistKey: string;
+  persistVersion: number;
 }
 
 export const PersistProvider = ({
   children,
   defaultValue,
   persistKey,
+  persistVersion,
 }: PersistProviderProps): JSX.Element => {
-  const key = useMemo(() => `react-persist-${persistKey}`, [persistKey]);
-  const [value, setValue] = useState(localStorage.getItem(key) ?? defaultValue);
+  const storedInfo = JSON.parse(localStorage.getItem('react-persist') ?? '{}');
+  // object will have keys of persistKeys in it, within that persistKey will be an object with a value and a version
+  const [value, setValue] = useState(storedInfo[persistKey] ?? defaultValue);
 
   useEffect(() => {
-    localStorage.setItem(key, value);
+    const updatedStoredInfo = {
+      ...storedInfo,
+      [persistKey]: { ...value[persistKey] },
+    };
+    localStorage.setItem('react-persist', updatedStoredInfo);
   }, [value, key]);
+
+  useEffect(() => {
+    localStorage.setItem(keyVersion, persistVersion.toString());
+  }, [keyVersion, persistVersion]);
+
+  useEffect(() => {
+    if (
+      Number.parseInt(localStorage.getItem(keyVersion) ?? '0') !==
+      persistVersion
+    ) {
+      localStorage.setItem(keyVersion, persistVersion.toString());
+      localStorage.setItem(key, defaultValue);
+    }
+  }, [defaultValue, key, keyVersion, persistVersion]);
 
   return (
     <PersistValueContext.Provider value={value}>
